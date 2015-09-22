312b3a81b7e331f03a95590653cab0b8eb18f93f
Alex <alex@Danis-MacBook-Pro.local>
2015-09-22 13:04:44 +0800

    Initial Commit

---
+++ gittest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gittest
+//
+//  Created by Alex on 22/09/15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
