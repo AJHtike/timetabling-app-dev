3fc2bca8110778c27b0bc0537d0534150068fcc5
Alex <alex@Danis-MacBook-Pro.local>
2015-09-22 13:07:34 +0800

    hello

--- gittest/main.m
+++ gittest/main.m
@@ -12,6 +12,7 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSlog(@"hello hello");
     }
     return 0;
 }
