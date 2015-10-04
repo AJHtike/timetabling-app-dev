1d05e1c8d9f67226eefd5b86be10a0fbd00e77b7
AJHtike <khunhak@gmail.com>
2015-10-04 12:04:45 +0800

    this is second time test

--- gittest/main.m
+++ gittest/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!"); //Hey this is commit
+        NSLog(@"Hello, World!"); //Hey this is commit second time
     }
     return 0;
 }
