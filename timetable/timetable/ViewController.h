99b852ca2a2214e616b11d9559720ab97ca2a22f
AJHtike <khunhak@gmail.com>
2015-10-04 13:46:39 +0800

    initial commit

---
+++ timetable/timetable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  timetable
+//
+//  Created by Alex on 04/10/15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ timetable/timetable/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  timetable
+//
+//  Created by Alex on 04/10/15.
+//  Copyright (c) 2015 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
