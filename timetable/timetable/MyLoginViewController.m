127b9c6272ab8709c7c1d52950111f203216e7a2
Andries De Klerk <aimeeaj.durand@gmail.com>
2015-10-29 09:01:36 +0800

    better folder structure
    updated the app logo
    Organised files into proper folders

---
+++ timetable/timetable/MyLoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  MyLoginViewController.m
+//  timetable
+//
+//  Created by Andries De Klerk on 27/10/2015.
+//  Copyright © 2015 Alex. All rights reserved.
+//
+
+#ifndef MyLoginViewController_h
+#define MyLoginViewController_h
+
+
+#endif /* MyLoginViewController_h */
